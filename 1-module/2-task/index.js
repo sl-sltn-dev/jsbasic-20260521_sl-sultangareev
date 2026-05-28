419105a0fcd745ab9fd96919ea97d17e2eae09b2
SLTN <sl.sultangareev@bk.ru>
2026-05-28 14:32:34 +0300

    isvalid - ok

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -8,15 +8,20 @@ function print(text) {
  */
 
 function isValid(name) {
+  /**
+   * у меня не получилось указать условие в if в таком виде: name !== null &&
+   * поэтому вынес его в отдельный if
+   * можете объяснить, пожалуйста, почему так?
+   */
 
   if (name === null) {
     return false
   }
 
   type_name = typeof name;
+  name_includes = name.includes(" ")
   length_name = name.length;
   type_length_name = typeof length_name;
-  name_includes = name.includes(" ")
 
   console.log(`${name_includes}`)
   console.log(`${name}`);
@@ -24,7 +29,7 @@ function isValid(name) {
   console.log(`${length_name}`);
   console.log(`${type_length_name}`);
 
-  if (name_includes != true && length_name >= 4 && type_name === "string" && name !== "") {
+  if (name_includes != true && length_name >= 4 && type_name === "string") {
     return true
   } else {
     return false
